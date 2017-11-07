e6964d8d92b24ed36f0b6d50caccec7df9c75b9f
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-11-07 10:10:07 +0330

    add ViewRender to services

--- RazorCompiler/Startup.cs
+++ RazorCompiler/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorCompiler.Helpers;
 
 namespace RazorCompiler
 {
@@ -29,6 +30,8 @@ namespace RazorCompiler
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddTransient<ViewRenderService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
