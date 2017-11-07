ee08cba037efbff0ab899c85adca919ef4b6dd35
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-11-07 10:18:04 +0330

    implement renderer service

--- RazorCompiler/Controllers/HomeController.cs
+++ RazorCompiler/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RazorCompiler.Helpers;
+using RazorCompiler.Models;
 
 namespace RazorCompiler.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ViewRenderService _renderService;
+
+        public HomeController(ViewRenderService renderService)
+        {
+            _renderService = renderService;
+        }
+
         public IActionResult Index()
         {
-            return Content("Hello World");
+            var model = new World { Title = "Earth" };
+            var compiled = _renderService.Render("Views/_SampleRazorTemplate.cshtml", model);
+            return Content(compiled);
         }
     }
 }
