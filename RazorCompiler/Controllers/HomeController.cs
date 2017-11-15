6f0b5523d5ff7183cd4d0cb696eda4bcf2a0efc0
Soren Zand <sunrise.freedom@gmail.com>
2017-11-15 11:43:23 +0330

    Compile razor templates as strings #1

--- RazorCompiler/Controllers/HomeController.cs
+++ RazorCompiler/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RazorCompiler.Helpers;
 using RazorCompiler.Models;
+using RazorLight;
+using RazorLight.Extensions;
 
 namespace RazorCompiler.Controllers
 {
@@ -17,11 +19,27 @@ namespace RazorCompiler.Controllers
             _renderService = renderService;
         }
 
-        public IActionResult Index()
+        public IActionResult FileIndex()
         {
             var model = new World { Title = "Earth" };
             var compiled = _renderService.Render("Views/_SampleRazorTemplate.cshtml", model);
             return Content(compiled);
+        }
+
+        public IActionResult Index()
+        {
+            const string content = "Hello @Model.Name. Welcome to @Model.Title repository";
+            var engine = EngineFactory.CreatePhysical(@"c:\Windows" /*it's fake, not needed and going to fix in 2.0.0*/);
+
+            var model = new
+            {
+                Name = "Posht-Kohi",
+                Title = "RazorLight"
+            };
+
+            string result = engine.ParseString(content, model);
+
+            return Content(result);
         }
     }
 }
